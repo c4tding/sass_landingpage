b678480e87f4991a2f47b101feae1b2e9b1f3253
Ilnohen <ilhamnopi@icloud.com>
2025-10-03 00:00:53 +0700

    feat add faq items structure inside FAQ layout - prepare for accordion interaction.

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -12,11 +12,26 @@ const FaqItem = ({ item, index }) => {
             <div className="group relative flex cursor-pointer items-center justify-between gap-10 px-7"
                 onClick={() => {
                     setActiveId(activeId === item.id ? null : item.id)
-                }}>
-                <div className="small-compact mb-1.5 text-p3  max-lg:hidden">
-                    {index < 10 ? '0' : ''}{index}
+                }}
+            >
+                <div className="flex-1  ">
+                    <div className="small-compact mb-1.5 text-p3  max-lg:hidden">
+                        {index < 10 ? '0' : ''}
+                        {index}
+                    </div>
+                    <div className={clsx("text-h6 font-h6 leading-h6 text-p4 transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center", active && "max-lg:text-p1")}>
+                        {item}
+                    </div>
                 </div>
-                <div className={clsx("text-h6 font-h6 leading-h6 text-p4 transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center", active && "max-lg:text-p1")}>{item}</div>
+
+                <div
+                    className={clsx(
+                        "faq-icon relative flex size-12 items-center justify-center rounded-full border-2 border-s2 shadow-400 transition-all duration-500 group-hover:border-s4",
+                        active && "faq-icon_active",
+                    )}
+                >
+                    <div className="g4 size-11/12 rounded-full shadow-300" />
+                </div>
             </div>
         </div>
     )
--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,13 +1,31 @@
 
 import { Element } from 'react-scroll';
+import FaqItem from '../components/FaqItem';
+import { faq } from '../constants/index';
 const Faq = () => {
+    const halfLength = Math.floor(faq.length / 2)
     return (
         <section>
             <Element name='faq' className='relative'>
                 <div className="container relative z-2 py-28">
                     <div>
-                        <h3 className='text-h3 font-h3 leading-h3 tracking-h3 max-md:text-h5 max-md:font-h5 max-md:leading-h5 max-w-640 max-lg:max-w-md mb-7 text-p4'>Curiosity didn't kill the cat, it given answers. </h3>
+                        <h3 className='text-h3 font-h3 leading-h3 tracking-h3 max-md:text-h5 max-md:font-h5 max-md:leading-h5 max-w-640 max-lg:max-w-md mb-7 text-p4'>Curiosity didn't kill the cat, it given answers.
+                        </h3>
                         <p className='text-body-1 leading-body-1 max-lg:max-w-sm'>You've got questions, we've got answers.</p>
+                    </div>
+
+                    <div className='faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2' />
+                </div>
+                <div className='faq-glow_before relative z-2 border-2 border-s2 bg-s1'>
+                    <div className="container flex gap-10 max-lg:block">
+                        <div className="rounded-half absolute -top-10 left-[calc(50%-39.9px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1">
+                            <img src="/images/faq-logo.svg" alt="logo" className='size-1/2' />
+                        </div>
+                        <div className="relative flex-1 pt-24">
+                            {faq.slice(0, halfLength).map((item, index) => (
+                                <FaqItem key={item.id} item={item.question} index={index} />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </Element>
