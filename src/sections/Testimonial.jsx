7067aafed2f6ce8b7ea33e16a410f4efedfc8a3b
Ilnohen <ilhamnopi@icloud.com>
2025-10-05 21:10:15 +0700

    feat add header Testimonial

--- src/sections/Testimonial.jsx
+++ src/sections/Testimonial.jsx
@@ -1,7 +1,14 @@
 
 const Testimonial = () => {
     return (
-        <div>Testimonial</div>
+        <section className="relative z-2 py-24 md:py-28 lg:py-40">
+            <div className="container block lg:flex">
+                <div className="testimonials_head-res relative z-2 mr-20 flex-300">
+                    <p className="caption mb-5 max-md:mb-2.5">Wall of love</p>
+                    <h3 className="text-h3 font-h3 leading-h3 tracking-h3 max-md:text-h5 max-md:font-h5 max-md:leading-h5 max-w-640 max-lg:max-w-md mb-7 text-p4">Words from our fans</h3>
+                </div>
+            </div>
+        </section>
     )
 }
 
